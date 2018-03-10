42827a7e0aef0e02c7e7cfb5d201c07c7eb37382
Allen Shi <shijl@cn.ibm.com>
2018-03-10 23:04:21 +0800

    remove useless import

--- sava-fragrans/src/main/java/net/sjl/fragrans/fs/ApacheFileSystemWatcher.java
+++ sava-fragrans/src/main/java/net/sjl/fragrans/fs/ApacheFileSystemWatcher.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
