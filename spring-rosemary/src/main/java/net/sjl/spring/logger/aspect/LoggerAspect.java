3924cbb16b1623408a04b1f1cfd1fdca960492a2
胜庸 <shengyong.sjl@alibaba-inc.com>
2019-05-13 20:08:10 +0800

    Print log to verify the aspect bean was created during the startup of application

--- spring-rosemary/src/main/java/net/sjl/spring/logger/aspect/LoggerAspect.java
+++ spring-rosemary/src/main/java/net/sjl/spring/logger/aspect/LoggerAspect.java
@@ -1,8 +1,11 @@
 package net.sjl.spring.logger.aspect;
 
+import javax.annotation.PostConstruct;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -14,7 +17,13 @@ public class LoggerAspect {
     long start = System.currentTimeMillis();
     Object proceed = joinPoint.proceed();
     long duration = System.currentTimeMillis() - start;
-    System.out.println(joinPoint.getSignature() + " executed in " + duration + "ms");
+    System.out.println("-----> " + joinPoint.getSignature() + " was executed within " + duration + "ms");
     return proceed;
+  }
+  
+  
+  @PostConstruct
+  public void postCreation() {
+	  System.out.println("-----> LoggerAspect bean is created");
   }
 }
